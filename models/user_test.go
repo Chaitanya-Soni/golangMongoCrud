0fd92da82b08593e9a836f48d0d270d582da336a
CHAITANYA SONI <54990096+Chaitanya-Soni@users.noreply.github.com>
2022-12-29 20:26:17 +0530

    initial update

---
+++ models/user_test.go
@@ -0,0 +1,67 @@
+package models
+
+import (
+	"github.com/mongo-go/testdb"
+	"testing"
+	"time"
+)
+
+
+func setUp(t *testing.T){
+	testDb := testdb.NewTestDB("mongodb://localhost:27017", "test", time.Duration(10) * time.Second)
+	testDb.Connect()
+	coll, err := testDb.CreateRandomCollection(testdb.NoIndexes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	collection = coll
+}
+
+
+var u = User{
+}
+
+func TestDeleteUserById(t *testing.T) {
+	setUp(t)
+	user,err := u.CreateUser()
+	if err !=nil {
+		t.Error("Fail to create user")
+	}
+	err = DeleteUserById(user.ID.Hex());
+	if err !=nil {
+		t.Error("Fail to delete user")
+	}
+}
+
+func TestGetUserById(t *testing.T) {
+	setUp(t)
+	user,err := u.CreateUser()
+	if err !=nil {
+		t.Error("Fail to create user")
+	}
+	_,err = GetUserById(user.ID.Hex())
+	if err != nil {
+		t.Error("Fail to get user")
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	setUp(t)
+	user,err := u.CreateUser();
+	if(err !=nil){
+		t.Error("Fail to create user")
+	}
+	user.Name ="peter"
+	err = UpdateUser(user); if(err !=nil){
+		t.Error("Fail to update user")
+	}
+
+}
+
+func TestUser_CreateUser(t *testing.T) {
+	setUp(t)
+	_,err := u.CreateUser();
+	if err !=nil {
+		t.Error("Fail to create user")
+	}
+}
